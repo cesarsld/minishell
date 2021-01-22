065bc55758413def9bd07d5bb73ce0fa1c31d9a1
cesarsld <jaimes.cesar88@gmail.com>
2021-01-22 15:55:10 +0100

    handle double slash

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: cjaimes <cjaimes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 15:18:17 by cjaimes           #+#    #+#             */
-/*   Updated: 2021/01/22 15:50:03 by cjaimes          ###   ########.fr       */
+/*   Updated: 2021/01/22 15:54:13 by cjaimes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ char	*ft_strpathjoin(char const *s1, char const *s2)
 	if (!(new = malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	ft_strlcpy(new, s1, len + 1);
+	if (*(s1 + ft_strlen(s1) - 1) == '/')
+		*(new + ft_strlen(s1) - 1) = 0;
 	ft_strlcat(new, "/", len + 1);
 	ft_strlcat(new, s2, len + 1);
 	return (new);
