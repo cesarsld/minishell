4d8248dd0ac5631b42038ceb507ea37ea9cf0bda
cesarsld <jaimes.cesar88@gmail.com>
2021-01-28 20:23:01 +0100

    remove unused var

--- srcs/gnl_shell.c
+++ srcs/gnl_shell.c
@@ -6,18 +6,17 @@
 /*   By: cjaimes <cjaimes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 20:19:43 by cjaimes           #+#    #+#             */
-/*   Updated: 2021/01/28 20:20:13 by cjaimes          ###   ########.fr       */
+/*   Updated: 2021/01/28 20:22:54 by cjaimes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		scan_buf_shell(char **line, char *buf, ssize_t size, int check)
+int		scan_buf_shell(char **line, char *buf, ssize_t size)
 {
 	int i;
 
 	i = 0;
-	check = 0;
 	if (size == -1)
 		return (-1);
 	if ((i = check_line(line, buf, size)) == -1)
@@ -34,7 +33,6 @@ int		get_next_line_shell(int fd, char **line)
 	static char	buf[BUFFER_SIZE + 1];
 	ssize_t		size;
 	int			ret;
-	int			check;
 
 	if (fd < 0 || !(*line = malloc(sizeof(char))) || BUFFER_SIZE <= 0)
 		return (-1);
@@ -42,13 +40,9 @@ int		get_next_line_shell(int fd, char **line)
 	size = 1;
 	while (1)
 	{
-		check = 0;
 		if ((size = ft_strlen(buf)) == 0)
-		{
 			size = read(fd, buf, BUFFER_SIZE);
-			check = 1;
-		}
-		if ((ret = scan_buf_shell(line, buf, size, check)) < 2)
+		if ((ret = scan_buf_shell(line, buf, size)) < 2)
 			return (ret);
 	}
 	return (0);
